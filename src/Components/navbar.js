724114234e1471f86b1ab520b56641f4d49bc77e
Ahmed Hamed <tunashark@gmail.com>
2020-08-31 17:46:38 +0100

    added basic links to navbar

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -4,7 +4,19 @@ import {Link} from 'react-router-dom';
 const NavBar = (props) => {
 
     return (
-        null
+        <>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/projects">Projects</Link>
+                </li>
+            </ul>
+        </>
     )
 
 }
